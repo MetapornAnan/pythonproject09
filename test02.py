0e1150b0a456c05d07ee77edfb0fc9a508371a76
MetapornAnan <s6519410054@sau.ac.th>
2023-11-03 11:53:20 +0700

    code v.2 : create test03-04 complete ja

--- test02.py
+++ test02.py
@@ -3,7 +3,7 @@ class IoTThailand :
     wow = 100
     wea = ''
 
-    #construtor ไม่ใช่ member แต่จะทำงานทุกครั้งที่มีการสร้าง object ไม่มีก็ได้
+    #constructor ไม่ใช่ member แต่จะทำงานทุกครั้งที่มีการสร้าง object ไม่มีก็ได้
     def __init__(self, woo, wee, wea):
         self.woo = woo
         self.wee = wee
@@ -12,6 +12,10 @@ class IoTThailand :
     #method
     def showData(self) :
         print(self.wow * 20)
+
+    #destructor ไม่ใช่ member แต่จะทำงานทุกครั้งที่ member ทำงานเสร็จ (ถูกทำลายทิ้ง)
+    def __del__(self):
+        print('Good Morning Teacher.....')
 
 # อย่าลืมส่ง argrument ให้ parameter ด้วย
 ob1 = IoTThailand(10, 20, 10)
